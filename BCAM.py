bb40814a743118644c32058efffa3704c5a210dd
vkumar-ucd <vaibhav.kumar@ucdconnect.ie>
2022-07-08 17:42:57 +0100

    minor updates

--- BCAM.py
+++ BCAM.py
@@ -8,7 +8,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-# np.random.seed(1)
+np.random.seed(1)
 
 
 # Function for dB to power conversion
@@ -31,14 +31,85 @@ def chanGen(zeta,d,dim1,dim2):
             +1j*np.random.randn(dim1,dim2))         # Rayleigh distributed random variables 
     return y    
 
-# Function to compute f(phi_l)
-def fCalc(phi):
+# Function to compute the objective in (15)
+def gCalc(phi,r_bl,phi_bl,beta_bl,r_el,phi_el,beta_el):
     numLocal = r_bl*np.cos(phi-phi_bl)+beta_bl    
     denLocal = r_el*np.cos(phi-phi_el)+beta_el
     return numLocal/denLocal
 
+# Function to update w
+def updateW(theta):
+    zb = hib@np.diag(thetaCurrent)@Hai+hab          # effective Alice-Bob channel       
+    ze = hie@np.diag(thetaCurrent)@Hai+hae          # effective Alice-Eve channel
+    Zb = Herm(zb)@zb                                # see below (7)
+    Ze = Herm(ze)@ze                                # see below (7)
+    tempMat = np.linalg.pinv(P*Ze+np.eye(N))@\
+                    (P*Zb+np.eye(N))                # see below (8)
+    eigVals,eigVecs = np.linalg.eig(tempMat)        # EigenValue Decomposition
+    maxCol = list(eigVals).index(max(eigVals))      # index of the maximum eigenvalue
+    uMax = eigVecs[:,[maxCol]]                      # eigenvector corresponding to largest eigenvalue
+    uMax = uMax/np.linalg.norm(uMax,2)              # normalizing the eigenvector
+    w = np.sqrt(P)*uMax                             # updated transmit beamformer
+    return w 
 
+# Function to update RIS phase shifts
+def updateTheta(w,theta):
+    # computing paramters below (10)
+    ab = np.diag(np.ndarray.flatten(hib.conj()))@Hai.conj()@w.conj()
+    bb = ab@(w.T)@(hab.T)
+    cb = (abs(hab@w))**2
+    # computing parameters below (12)
+    ae = np.diag(np.ndarray.flatten(hie.conj()))@Hai.conj()@w.conj()
+    be = ae@(w.T)@(hae.T)
+    ce = (abs(hae@w))**2    
+    
+    for l in range(Nris):
+        theta_m = np.delete(theta,l)
+        # parameters related to Bob
+        ab_l = ab[l]
+        ab_m = np.delete(ab,l)
+        bb_l = bb[l]
+        bb_m = np.delete(bb,l)        
+        alpha_bl = 2*(ab_l*np.sum(ab_m.conj()*theta_m)+bb_l)
+        beta_bl = abs(ab_l)**2+abs(np.sum(ab_m.conj()*theta_m))**2\
+                            +2*np.real(np.sum(bb_m.conj()*theta_m))+cb+1
+        r_bl = abs(alpha_bl)
+        phi_bl = np.angle(alpha_bl)
+        # parameters related to Eve
+        ae_l = ae[l]
+        ae_m = np.delete(ae,l)
+        be_l = be[l]
+        be_m = np.delete(be,l)
+        alpha_el = 2*(ae_l*np.sum(ae_m.conj()*theta_m)+be_l)
+        beta_el = abs(ae_l)**2+abs(np.sum(ae_m.conj()*theta_m))**2\
+                            +2*np.real(np.sum(be_m.conj()*theta_m))+ce+1        
+        r_el = abs(alpha_el)
+        phi_el = np.angle(alpha_el)
+        # obtaining optimal phase-shift for the l-th tile 
+        r_l = np.sqrt((r_bl*beta_el)**2 + (r_el*beta_bl)**2\
+             - 2*r_bl*r_el*beta_bl*beta_el*np.cos(phi_el-phi_bl))       # see below (16)
+        num = -r_bl*beta_el*np.sin(phi_bl)+r_el*beta_bl*np.sin(phi_el)
+        den = r_bl*beta_el*np.cos(phi_bl)-r_el*beta_bl*np.cos(phi_el)
+        varphi_l = np.arctan2(num,den)                                  # see above (17)
+        phi_l0 = np.array([0])
+        phi_l1 = np.arcsin((r_bl*r_el/r_l)\
+                    *np.sin(phi_bl-phi_el))-varphi_l                    # see (18)
+        phi_l2 = np.pi-np.arcsin((r_bl*r_el/r_l)\
+                    *np.sin(phi_bl-phi_el))-varphi_l                    # see (18)
+        phi_l_Vec = np.concatenate((phi_l0,phi_l1.flatten(),\
+                        phi_l2.flatten()),axis=0)                       # list of possible phase-shifts
+        maxIndex = np.argmax(gCalc(phi_l_Vec,r_bl,phi_bl,\
+                        beta_bl,r_el,phi_el,beta_el))                   # index of optimal phase-shift in the list
+        phi_l_opt = phi_l_Vec[maxIndex]                                 # optimal phase-shift for the l-th tile
+        theta[l] = np.exp(1j*phi_l_opt)                                 # optimal reflection-coefficient for the l-th tile
+    return theta
 
+# Function to calculate secrecy rate
+def rateCalc(w,theta):
+    zb = hib@np.diag(theta)@Hai+hab
+    ze = hie@np.diag(theta)@Hai+hae
+    y = np.log2(1+abs(zb@w)**2)-np.log2(1+abs(ze@w)**2)
+    return y   
 
 # System parameters
 P = db2pow(15)                          # transmit power at Alice
@@ -59,7 +130,8 @@ dAE = np.sqrt(dAEh**2 + dv**2)          # distance between Alice and Eve
 dIE = np.sqrt((dAI-dAEh)**2 + dv**2)    # distance between RIS and Eve
 dIB = np.sqrt((dAI-dABh)**2 + dv**2)    # distance between RIS and Eve 
 rateSeq = np.array([])                  # empty list to store the rate sequence 
-maxIter = 100                           # maximum number of iterations
+relChange = 1e3                         # arbitrary large number
+epsilon = 1e-5                          # convergence tolerance
 
 # Generating the channel coefficients
 normFact = 1/np.sqrt(sigmaSquare)           # normalization factor
@@ -72,64 +144,22 @@ hae = normFact*chanGen(zetaAE,dAE,1,N)      # Alice-Eve channel
 # Random initialization
 thetaCurrent = np.ones((Nris,),dtype=complex)
 
-# Effective channels
-zb = hib@np.diag(thetaCurrent)@Hai+hab
-ze = hie@np.diag(thetaCurrent)@Hai+hae
-
 # Algorithm 1: Block-Coordinate Ascent Method
-for iIter in range(maxIter):    
-    Zb = Herm(zb)@zb
-    Ze = Herm(ze)@ze
-    tempMat = np.linalg.pinv(P*Ze+np.eye(N))@(P*Zb+np.eye(N))
-    eigVals,eigVecs = np.linalg.eig(tempMat)
-    maxCol = list(eigVals).index(max(eigVals))
-    uMax = eigVecs[:,[maxCol]]
-    uMax = uMax/np.linalg.norm(uMax,2)
-    wUpdated = np.sqrt(P)*uMax
-    ab = np.diag(np.ndarray.flatten(hib.conj()))@Hai.conj()@wUpdated.conj()
-    bb = ab@(wUpdated.T)@(hab.T)
-    cb = (abs(hab@wUpdated))**2
-    ae = np.diag(np.ndarray.flatten(hie.conj()))@Hai.conj()@wUpdated.conj()
-    be = ae@(wUpdated.T)@(hae.T)
-    ce = (abs(hae@wUpdated))**2    
-    
-    for nris in range(Nris):
-        theta_l = thetaCurrent[nris]
-        theta_m = np.delete(thetaCurrent,nris)
-        ab_l = ab[nris]
-        ab_m = np.delete(ab,nris)
-        bb_l = bb[nris]
-        bb_m = np.delete(bb,nris)
-        ae_l = ae[nris]
-        ae_m = np.delete(ae,nris)
-        be_l = be[nris]
-        be_m = np.delete(be,nris)
-        alpha_bl = 2*(ab_l*np.sum(ab_m.conj()*theta_m)+bb_l)
-        beta_bl = abs(ab_l)**2+abs(np.sum(ab_m.conj()*theta_m))**2+2*np.real(np.sum(bb_m.conj()*theta_m))+cb+1
-        alpha_el = 2*(ae_l*np.sum(ae_m.conj()*theta_m)+be_l)
-        beta_el = abs(ae_l)**2+abs(np.sum(ae_m.conj()*theta_m))**2+2*np.real(np.sum(be_m.conj()*theta_m))+ce+1
-        r_bl = abs(alpha_bl)
-        phi_bl = np.angle(alpha_bl)
-        r_el = abs(alpha_el)
-        phi_el = np.angle(alpha_el)
-        phi_l = np.angle(theta_l)
-        r_l = np.sqrt((r_bl*beta_el)**2 + (r_el*beta_bl)**2\
-             - 2*r_bl*r_el*beta_bl*beta_el*np.cos(phi_el-phi_bl))
-        num = -r_bl*beta_el*np.sin(phi_bl)+r_el*beta_bl*np.sin(phi_el)
-        den = r_bl*beta_el*np.cos(phi_bl)-r_el*beta_bl*np.cos(phi_el)
-        varphi_l = np.arctan2(num,den)
-        phi_l0 = np.array([0])
-        phi_l1 = np.arcsin((r_bl*r_el/r_l)*np.sin(phi_bl-phi_el))-varphi_l
-        phi_l2 = np.pi-np.arcsin((r_bl*r_el/r_l)*np.sin(phi_bl-phi_el))-varphi_l
-        phi_l_Vec = np.concatenate((phi_l0,phi_l1.flatten(),phi_l2.flatten()),axis=0)
-        maxIndex = np.argmax(fCalc(phi_l_Vec))
-        phi_l_opt = phi_l_Vec[maxIndex]
-        theta_l = np.exp(1j*phi_l_opt)
-        thetaCurrent[nris] = theta_l
-    zb = hib@np.diag(thetaCurrent)@Hai+hab
-    ze = hie@np.diag(thetaCurrent)@Hai+hae
-    rateSeq = np.append(rateSeq,np.log2(1+abs(zb@wUpdated)**2)-np.log2(1+abs(ze@wUpdated)**2))
+while relChange > epsilon:  
+    # update transmit beamformer 
+    wCurrent = updateW(thetaCurrent) 
+    # update RIS phase shifts
+    thetaCurrent = updateTheta(wCurrent,thetaCurrent) 
+    # calculate rate
+    rateSeq = np.append(rateSeq,rateCalc(wCurrent,thetaCurrent))
+    # check for convergence
+    if len(rateSeq) > 3:
+        relChange = (rateSeq[-1] - rateSeq[-2])/rateSeq[-2]
+
+# Plot the iterates 
 plt.plot(rateSeq)
+plt.xlabel('Iteration number')
+plt.ylabel('Secrecy rate (bps/Hz)')
 plt.show()
         
 
